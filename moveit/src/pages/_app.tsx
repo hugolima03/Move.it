ed398436580cd28e80e1d4e63cb589e86f4d924c
Hugo Lima Romão <hugo8romao@gmail.com>
2021-02-24 09:18:53 -0400

    criando contexto de desafios

--- moveit/src/pages/_app.tsx
+++ moveit/src/pages/_app.tsx
@@ -1,7 +1,17 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
 import '../styles/global.css';
 
+// eslint-disable-next-line import/extensions
+import { ChallengesProvider } from '../contexts/ChallengesContext';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
